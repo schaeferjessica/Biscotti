3ce8e8d078eafe05d21bab7c10c62c76d57b164c
jschaefer <jschaefer@3pc.de>
2021-01-23 00:09:31 +0100

    feat: create-strapi-app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-biscotti
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '27b1d373-f149-497a-bf8f-b589766eb04e'
+};
