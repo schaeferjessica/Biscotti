2552b3775e6ae238e4fdb889861297aef4c29dce
jschaefer <jschaefer@3pc.de>
2021-01-23 16:13:11 +0100

    feat(footer): add links to footer component

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,19 +1,89 @@
 import React from 'react';
-import styled from 'styled-components';
-import { moduleSpace } from '../styles/container';
+import styled, { keyframes } from 'styled-components';
+import { devices } from '../styles/breakpoints';
 
-export const FooterEl = styled.footer`
-  ${moduleSpace}
+const backgroundWidth = '92px';
+const wave = keyframes`
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: ${backgroundWidth} 0;
+  }
+`;
+
+export const NavFooter = styled.footer`
   display: flex;
   justify-content: flex-end;
-  margin-bottom: 20px;
+  width: 100%;
+  margin-top: 130px;
+
+  @media ${devices.mobile} {
+    margin-top: 40px;
+  }
+
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    display: block;
+    position: relative;
+    padding-bottom: 10px;
+    text-align: center;
+
+    &:hover::after,
+    &:focus::after {
+      animation: 2s ${wave} linear infinite;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      width: ${backgroundWidth};
+      height: 8px;
+      transform: translateX(-50%);
+      background-image: url('./images/line.svg');
+      background-size: contain;
+      background-repeat: repeat-x;
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-bottom: 70px;
+
+    @media ${devices.mobile} {
+      margin-bottom: 40px;
+    }
+  }
+
+  li {
+    margin-left: 100px;
+  }
 `;
 
 const Footer = () => {
   return (
-    <FooterEl className="container">
-     <p>footer</p>
-    </FooterEl>
+    <div className="container">
+      <NavFooter>
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/biscotti.eis/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="#id">Impressum</a>
+          </li>
+        </ul>
+      </NavFooter>
+    </div>
   );
 };
 
